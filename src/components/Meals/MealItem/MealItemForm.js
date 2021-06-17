3bb2d45f39acaad71dfc479dfa3a7eafa397ad50
Kevin Knebel <kevin.knebel@usu.com>
2021-06-17 14:05:29 +0200

    adding refs and forwarding refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 
 import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if (
+            enteredAmount.trim().length === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmountNumber > 5
+        ) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount"
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                label="Amount"
+                ref={amountInputRef}
                 input={{
                     id: "amount_" + props.id,
                     type: "number",
@@ -17,6 +39,7 @@ const MealItemForm = (props) => {
                 }}
             />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount!</p>}
         </form>
     );
 };
